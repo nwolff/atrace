e9722635456bf5573620af06a9c699c7f27b660e
Nicholas Wolff <nwolff@gmail.com>
2025-12-14 10:53:03 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,66 @@
+# TODO:
+
+- ignore function definitions (look at the type of object)
+
+- display at end only
+
+- entering a function, binding the local arguments, returning
+
+# Usage
+
+Automatically prints a trace table of a program once the execution is finished.
+
+Just import the module.
+
+An animated example of a trace table: https://www.101computing.net/using-trace-tables/
+
+# Requirements
+
+- Should not interfere with the running program (apart from capturing stdout)
+- Should display the trace at the end of execution (not while the program is interacting with the user)
+- Should display the trace even if an exception interrupts the program
+- Should display the trace even if the user interrupts the program
+- Should handle mutations to objects like lists
+- Should handle functions properly:
+  - entering the function
+  - binding the local arguments
+  - returning
+
+# Supported environments
+
+- Thonny, which adds a shitload of magic
+- python 3.10+
+
+# Not in scope
+
+- Multithreaded programs
+
+# Implementation
+
+To trace variables :
+
+- Either https://docs.python.org/3/library/sys.html#sys.settrace
+- Or https://docs.python.org/3/library/trace.html
+
+To capture stdout :
+
+- Either https://docs.python.org/3/library/contextlib.html#contextlib.redirect_stdout
+- Or just reassign stdout
+
+To trap sigint:
+
+- https://stackoverflow.com/questions/1112343/how-do-i-capture-sigint-in-python
+
+# Build
+
+Automatically deployed to pypi every time a new tag is pushed: https://pypi.org/project/atrace/
+
+# Technical Refs
+
+- First similar thing I found, doesn't work (chokes on deep copying some variables): https://github.com/DarshanLakshman/PyTracerTool
+
+- 11 years old, doesn't work: https://github.com/mihneadb/python-execution-trace
+
+- A hot mess: https://stackoverflow.com/questions/1645028/trace-table-for-python-programs
+
+- A tutorial on the trace module: https://pymotw.com/2/trace/
---
+++ src/atrace/__init__.py
@@ -0,0 +1,97 @@
+import copy
+import inspect
+import sys
+from types import FrameType
+from typing import Any
+
+module_of_interest = None
+last_locals = {}
+
+
+def ignore_variable(name: str, value: Any):
+    # if name.startswith("_"):
+    #    return True
+    return callable(value)  # Functions, modules, tout ça
+
+
+def copy_carefully_using_ignored_variables(d: dict[str, Any]):
+    res = {}
+    for k, v in d.items():
+        if not ignore_variable(k, v):
+            res[k] = copy.deepcopy(v)
+    return res
+
+
+def copy_carefully(d: dict[str, Any]):
+    res = {}
+    for k, v in d.items():
+        try:
+            v_copy = copy.deepcopy(v)
+        except:
+            v_copy = v
+        res[k] = v_copy
+    return res
+
+
+def trace_vars(frame: FrameType, event: str, arg: Any):
+    if inspect.getmodule(frame) != module_of_interest:
+        return
+    if event != "line":
+        return trace_vars
+    code = frame.f_code
+    lineno = frame.f_lineno
+    locals_now = copy_carefully(frame.f_locals)
+    global last_locals
+
+    if last_locals is None:  # We're being unloaded, it's the end of the program
+        return None
+
+    if code.co_name not in last_locals:
+        last_locals[code.co_name] = locals_now
+        return trace_vars
+
+    old_locals = last_locals[code.co_name]
+
+    for var, new_val in locals_now.items():
+        if not ignore_variable(var, new_val):
+            if var not in old_locals:
+                print(f"[{lineno}] NEW {var} = {new_val}")
+            elif old_locals[var] != new_val:
+                print(f"[{lineno}] MODIFIED {var}: {old_locals[var]} → {new_val}")
+
+    for var, old_val in old_locals.items():
+        if var not in locals_now:
+            if not ignore_variable(var, old_val):
+                print(f"[{lineno}] DELETED {var}")
+
+    last_locals[code.co_name] = locals_now
+    return trace_vars
+
+
+def just_kicking_off(frame: FrameType, event: str, arg: Any):
+    return None
+
+
+def get_importer_frame():
+    # Get the current call stack
+    for frame_info in inspect.stack():
+        # Filter out internal importlib frames and the current module's frame
+        filename = frame_info.filename
+        if not filename.startswith("<") and filename != __file__:
+            return frame_info.frame
+    return None
+
+
+# It also kicks off the tracing machinery (so that the lines below work)
+sys.settrace(just_kicking_off)
+
+
+# This reaches into the importing module's frame to setup tracing of code outside of functions
+importer_frame = get_importer_frame()
+if importer_frame:
+    importer_frame.f_trace = trace_vars
+    module_of_interest = inspect.getmodule(importer_frame)
+    # This will work next time we enter a function.
+    sys.settrace(trace_vars)
+else:
+    print("Cannot trace")
